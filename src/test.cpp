f460e0f3b2efa0a2c688eec1f630029388bd4a5c
w@hxdl.org <w@hxdl.org>
2014-06-05 17:13:47 +0800

    fix serial

--- src/test.cpp
+++ src/test.cpp
@@ -7,12 +7,19 @@
 
 #include "serial.h"
 #include "engine.h"
+#include <iostream>
+#include <string>
 
 int main() {
 	hex::Plane p1;
-	hex::Serial uart("/dev/ttyAMA0");
-	p1.rotate(Eigen::Vector3f(0, 0, 1));
-	p1.writeSerial(uart);
+	try {
+		hex::Serial uart("/dev/ttyAMA0");
+		p1.rotate(Eigen::Vector3f(0, 0, 1));
+		p1.writeSerial(uart);
+	}
+	catch (std::string eObj) {
+		std::cerr << eObj << "\n";
+	}
 	return 0;
 }
 
