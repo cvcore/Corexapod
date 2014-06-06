364ab1590bcf1aee5f3037e01e17fe2e49101dda
w@hxdl.org <w@hxdl.org>
2014-06-06 09:18:40 +0800

    fix

--- src/test.cpp
+++ src/test.cpp
@@ -20,17 +20,18 @@ int main() {
 		Eigen::AngleAxisf rot(3.14f / 30.f, Eigen::Vector3f(0, 0, 1));
 		hex::Serial uart("/dev/ttyAMA0");
 		while(true) {
-//			std::cin >> x >> y >> z;
-//			norm = Eigen::Vector3f(x, y, z);
-//			p1.rotate(norm);
-//			p1.writeSerial(uart);
-//			std::cin.clear();
-			for(float t = 0; t < 3.14 * 2; t += 3.14 / 20) {
-				Eigen::Vector3f pos(150.8 + 5 * cos(t), 80.4 + 5 * sin(t), 0);
-				p1.leg_[0]->setPosition(pos, p1);
-				p1.writeSerial(uart);
-				usleep(200000);
-			}
+			std::cin >> x >> y >> z;
+			norm = Eigen::Vector3f(x, y, z);
+			p1.rotate(norm);
+			p1.writeSerial(uart);
+			std::cin.clear();
+			usleep(200000);
+//			for(float t = 0; t < 3.14 * 2; t += 3.14 / 20) {
+//				Eigen::Vector3f pos(150.8 + 5 * cos(t), 80.4 + 5 * sin(t), 0);
+//				p1.leg_[0]->setPosition(pos, p1);
+//				p1.writeSerial(uart);
+//				usleep(200000);
+//			}
 		}
 	}
 	catch (std::string eMsg) {
