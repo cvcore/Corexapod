13c2026cfb4f3c8a6308933eff2ef94487d3ffe3
w@hxdl.org <w@hxdl.org>
2014-06-18 16:20:04 +0800

    test

--- src/test.cpp
+++ src/test.cpp
@@ -19,6 +19,7 @@ int main() {
 //		hexapod.base_.leg_[1]->addMovement(Eigen::Vector3f(0, 133, 10), 200);
 //		hexapod.parseMovement();
 		hexapod.base_.translate(Eigen::Vector3f(0, 0, 120));
+		hexapod.base_.rotate(Eigen::Vector3f(1, 0, 5), Eigen::Vector3f(5, 0, -1));
 		hexapod.base_.writeSerial(hexapod.uart_);
 		usleep(500000);
 		hexapod.moveLinear(Eigen::Vector3f(50, 20, 0), 1000, 5);
