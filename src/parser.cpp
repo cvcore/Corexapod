81eab264eb6aa0694a21912ebe0e39b4df39a182
w@hxdl.org <w@hxdl.org>
2014-07-04 11:14:10 +0800

    p

--- src/parser.cpp
+++ src/parser.cpp
@@ -137,10 +137,11 @@ bool Parser::act(const std::string& methodName) {
 void Parser::parseLine(const Line& line) const {
 	std::vector<Group>::const_iterator it;
 	bool baseLine = false;
-	if(line.empty_)
-		usleep(line.time_ * 1000);
 	for(it = line.group_.begin(); it != line.group_.end(); it++) {
 		Eigen::Vector3f newVec(it->x_, it->y_, it->z_);
+		if(line.empty_) {
+			::usleep(line.time_ * 1000);
+		}
 		switch(it->groupType_) {
 		case 'B' :
 			baseLine = true;
@@ -177,7 +178,7 @@ void Parser::parseLine(const Line& line) const {
 	}
 	if(baseLine) {
 		_hexapod.base_.writeSerial(_hexapod.uart_);
-		::usleep(line.time_ * 1000);
+//		::usleep(line.time_ * 1000);
 	} else {
 		_hexapod.parseMovement();
 		const int allMembers[6] = {0, 1, 2, 3, 4, 5};
