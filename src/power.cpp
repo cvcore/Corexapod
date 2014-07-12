69e7fb122a3e0c718e4caf62d2da709e0bf51642
c0r3d3v <w@hxdl.org>
2014-07-12 22:03:29 +0800

    tested MCP3002 on rpi; fix #13

--- src/power.cpp
+++ src/power.cpp
@@ -28,16 +28,16 @@ PowerInterface::PowerInterface() {
 int PowerInterface::readBatteryPercentage() {
 	unsigned char buf[3];
 	const int lowest = 7.4f * 1024 / 8.2;
-	buf[0] = 0b01101000; //MCP3002 channel 0, MSB first
+	buf[0] = 0b11010000;//MCP3002 channel 0, MSB first
 	buf[1] = 0;
 	if(wiringPiSPIDataRW(0, buf, 2) == -1) {
 		std::cout << "[Power] SPI rw error.\n";
 		_adcValue = 1024;
 	} else {
-		//_adcValue = (int)buf[0] << 7 + buf[1] >> 1;
-		_adcValue = 1024; //change when module is availiable
+		_adcValue = ((buf[0] << 7) | (buf[1] >> 1)) & 0x3FF;
 	}
 	return (float)(_adcValue - lowest) / (1024 - lowest) * 100;
+	//return _adcValue;
 }
 
 void PowerInterface::servoPower(ServoPowerAction action) {
